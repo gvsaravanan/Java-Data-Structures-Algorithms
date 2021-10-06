93d4560b3e46aa8d2dc7e1febc0efe7d924a96b3
G-Saravanan <gautham.saravanan50@k12.leanderisd.org>
2021-10-06 11:50:04 -0500

    Update files

--- Computer Science 3/Probability & Searching Algorithms/src/RandomGaussian.java
+++
@@ -1,8 +0,0 @@
-import java.lang.*;
-
-public class RandomGaussian {
-    public static void main(String[] args) {
-        double r = Math.sin(2 * Math.PI * Math.random()) * Math.sqrt((-2 * Math.log(Math.random())));
-        System.out.println("Random Gaussian number: " + r);
-    }
-}
