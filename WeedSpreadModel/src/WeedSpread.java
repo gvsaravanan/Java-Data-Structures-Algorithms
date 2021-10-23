4541f2c9c368da481badaba743e31cd3d521bca7
G-Saravanan <gautham.saravanan50@k12.leanderisd.org>
2021-10-23 00:04:00 -0500

    Create WeedSpread.java

---
+++ WeedSpreadModel/src/WeedSpread.java
@@ -0,0 +1,244 @@
+import java.util.ArrayList;
+import java.awt.Color;
+import java.util.Arrays;
+
+public class WeedSpread {
+
+    private static int[][] arr;
+    private static String wind;
+
+    public WeedSpread(String w, int size) {
+        arr = new int[size][size];
+        this.wind = w;
+    }
+
+    public void initialize() {
+        for (int r = 0; r < arr.length; r++) {
+            for (int c = 0; c < arr[0].length; c++) {
+                arr[r][c] = 0;
+            }
+        }
+
+        int rand1 = (int) (Math.random() * arr.length - 1) + 1;
+        int rand2 = (int) (Math.random() * arr[0].length - 1) + 1;
+        System.out.println(rand1 + " " + rand2);
+        arr[rand1][rand2] = 1;
+        System.out.println(arr[rand1][rand2]);
+
+        rand1 = (int) (Math.random() * arr.length - 1) + 1;
+        rand2 = (int) (Math.random() * arr[0].length - 1) + 1;
+        System.out.println(rand1 + " " + rand2);
+        arr[rand1][rand2] = 2;
+        System.out.println(arr[rand1][rand2]);
+    }
+
+    public void draw(Picture p) {
+        for(int r = 0; r < p.width() - 1; r++) {
+            for (int c = 0; c < p.height() - 1; c++) {
+                if (arr[r][c] == 0) {
+                    p.set(r, c, new Color(193, 255, 157));
+                }
+                if (arr[r][c] == 1) {
+                    p.set(r, c, new Color(80, 178, 80));
+                }
+                if (arr[r][c] == 2) {
+                    p.set(r, c, new Color(0, 119, 0));
+                }
+            }
+        }
+        p.show();
+    }
+
+    public static boolean invasive(int r, int c){
+        boolean one = false;
+        boolean two = false;
+        boolean three = false;
+        boolean four = false;
+
+        while(!one && !two && !three && !four) {
+            double rand = Math.random() * 10;
+
+            if (rand <= 2) {
+                if (arr[r][c] != arr[(r - 1 + arr.length) % arr.length][(c + arr[0].length) % arr[0].length]) {
+                    return true;
+                }
+                one = true;
+            } else if (rand > 2 && rand <= 5) {
+                if (arr[r][c] != arr[(r + 1 + arr.length) % arr.length][(c + arr[0].length) % arr[0].length]) {
+                    return true;
+                }
+                two = true;
+            } else if (rand > 5 && rand <= 8) {
+                if (arr[r][c] != arr[(r + arr.length) % arr.length][(c - 1 + arr[0].length) % arr[0].length]) {
+                    return true;
+                }
+                three = true;
+            } else if (rand > 8 && rand < 10) {
+                if (arr[r][c] != arr[(r + arr.length) % arr.length][(c + 1 + arr[0].length) % arr[0].length]) {
+                    return true;
+                }
+                four = true;
+            }
+        }
+
+        return false;
+    }
+
+    /*
+    public void spread(int r, int c) {
+        int[] xMoves;
+        int[] yMoves;
+        switch (wind) {
+            case "North":
+                int[] x = {-1, -1, -1, 0, 0, 1, 1, 1};
+                int[] y = {-1, 0, 1, -1, 1, -1, 0, 1};
+                xMoves = x;
+                yMoves = y;
+                break;
+            case "East":
+                int[] x1 = {1, 0, -1, 1, -1, 1, 0, -1};
+                int[] y1 = {-1, -1, -1, 0, 0, 1, 1, 1};
+                xMoves = x1;
+                yMoves = y1;
+                break;
+            case "South":
+                int[] x2 = {1, 1, 1, 0, 0, -1, -1, -1};
+                int[] y2 = {-1, 0, 1, -1, 1, -1, 0, 1};
+                xMoves = x2;
+                yMoves = y2;
+                break;
+            case "West":
+                int[] x3 = {-1, 0, 1, -1, 1, -1, 0, 1};
+                int[] y3 = {-1, -1, -1, 0, 0, 1, 1, 1};
+                xMoves = x3;
+                yMoves = y3;
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + wind);
+        }
+        Boolean coordinate = arr[r][c];
+
+        for (int i = 0; i < xMoves.length; i++) {
+            if (((0 <= r + xMoves[i]) && (r + xMoves[i] < arr.length)) && ((0 <= c + yMoves[i]) && (c + yMoves[i] < arr[0].length))) {
+                if (invasive(r, c)) {
+                    arr[r][c] = true;
+                }
+            }
+        }
+    }
+
+     */
+
+    public void move(int r, int c) {
+        int[] xMoves;
+        int[] yMoves;
+        int coordinate = arr[r][c];
+
+        switch (wind) {
+            case "North":
+                int[] x = {-1, -1, -1, 0, 0, 1, 1, 1};
+                int[] y = {-1, 0, 1, -1, 1, -1, 0, 1};
+                xMoves = x;
+                yMoves = y;
+                break;
+            case "East":
+                int[] x1 = {-1, 0, 1, -1, 1, -1, 0, 1};
+                int[] y1 = {1, 1, 1, 0, 0, -1, -1, -1};
+                xMoves = x1;
+                yMoves = y1;
+                break;
+            case "South":
+                int[] x2 = {1, 1, 1, 0, 0, -1, -1, -1};
+                int[] y2 = {-1, 0, 1, -1, 1, -1, 0, 1};
+                xMoves = x2;
+                yMoves = y2;
+                break;
+            case "West":
+                int[] x3 = {-1, 0, 1, -1, 1, -1, 0, 1};
+                int[] y3 = {-1, -1, -1, 0, 0, 1, 1, 1};
+                xMoves = x3;
+                yMoves = y3;
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + wind);
+        }
+
+        for (int i = 0; i < xMoves.length; i++) {
+            if (((0 <= r + xMoves[i]) && (r + xMoves[i] < arr.length)) && ((0 <= c + yMoves[i]) && (c + yMoves[i] < arr[0].length))) {
+                if (coordinate == 1) {
+                    arr[r + xMoves[i]][c + yMoves[i]] = 1;
+                } else if (coordinate == 2) {
+                    arr[r + xMoves[i]][c + yMoves[i]] = 2;
+                }
+            }
+        }
+    }
+
+    public void spread() {
+        for(int r = 0; r < arr.length; r++){
+            for (int c = 0; c < arr[0].length; c++) {
+                if (invasive(r, c)){
+                    move(r, c);
+                }
+            }
+        }
+    }
+
+    public boolean empty() {
+        int counter = 0;
+        for (int r = 0; r < arr.length; r++) {
+            for (int c = 0; c < arr[0].length; c++) {
+                if (arr[r][c] == 0) {
+                    counter++;
+                }
+            }
+        }
+
+        return counter != 0;
+    }
+
+    /*
+    public double move() {
+        ArrayList<Integer> dissatisfied = new ArrayList<>();
+        double count = 0.0;
+
+        for (int r = 0; r < arr.length; r++) {
+            for (int c = 0; c < arr[0].length; c++) {
+                if (arr[r][c] == 0){
+                    dissatisfied.add(arr[r][c]);
+                } else if (!isSatisfied(r, c)) {
+                    dissatisfied.add(arr[r][c]);
+                    arr[r][c] = 0;
+                    count += 1.0;
+                }
+            }
+        }
+
+        for (int r = 0; r < arr.length; r++) {
+            for (int c = 0; c < arr[0].length; c++) {
+                if(arr[r][c] == 0){
+                    int index = (int) (Math.floor(Math.random() * dissatisfied.size()));
+                    arr[r][c] = dissatisfied.remove(index);
+                }
+            }
+        }
+
+        return ((arr.length * arr[0].length) - count) / (arr.length * arr[0].length);
+    }
+
+     */
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WeedSpread w = new WeedSpread("North", 500);
+        Picture p = new Picture(arr.length, arr[0].length);
+        w.initialize();
+
+        while (w.empty()){
+            w.draw(p);
+            w.spread();
+            Thread.sleep(1);
+        }
+    }
+
+}
