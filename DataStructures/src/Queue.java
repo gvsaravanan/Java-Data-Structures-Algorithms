7d46b4bc985b561f955c32b97d399206fdc384e3
G-Saravanan <gautham.saravanan50@k12.leanderisd.org>
2021-11-21 11:33:46 -0600

    Complete Queue.java

--- DataStructures/src/Queue.java
+++ DataStructures/src/Queue.java
@@ -1,3 +1,23 @@
-public class Queue {
+import java.util.LinkedList;
+import java.util.Queue;
 
+class QueueBasics {
+     public static void main(String[] args) {
+         Queue<Integer> queue = new LinkedList<>();
+         int ppl = 31;
+         int skips = 5;
+
+         for (int i = 1; i <= ppl; i++) {
+             queue.add(i);
+         }
+
+         while (!queue.isEmpty()) {
+             for (int i = 0; i < ppl; i++) {
+                 for (int j = 1; j < skips; j++) {
+                     queue.add(queue.remove());
+                 }
+                 System.out.println(queue.remove());
+             }
+         }
+     }
 }
